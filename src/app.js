ac1d078ec21a4b493ee4f2576a7bd8410961b329
nemoVim <nemovim@kentech.ac.kr>
2022-10-27 11:19:59 +0900

    Add helmet module

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
+
+
 import loginRouter from './routes/login.js';
 import userRouter from './routes/user.js';
 import gameRouter from './routes/game.js';
@@ -20,6 +23,7 @@ console.log(PORT, MONGO_URI);
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
 app.use(
     session({
         secret: 'abracadabra',
